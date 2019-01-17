bb5b318e618f61764720c1754ee9a865018b2d2a
rubinoAM <parkcathedralgrace@gmail.com>
2019-01-17 11:31:28 -0500

    Early implementation of request

---
+++ readme.md
@@ -0,0 +1,1 @@
+##Just a simple application made with Express
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const apiKey = require('../config');
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const imgBaseUrl = 'http://image.tmdb.org/t/p/w300';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+
+//$.getJSON(nowPlayingUrl,()=>{});
+const request = require('request');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  request.get(nowPlayingUrl,(err,resp,body)=>{
+    console.log(typeof(body));
+    const parsedData = JSON.parse(body);
+    console.log(parsedData);
+  });
   res.render('index', { title: 'Express' });
 });
 
