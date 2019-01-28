f678f841a0a2e84dc87076aca8608d4800a81b23
rubinoAM <parkcathedralgrace@gmail.com>
2019-01-28 09:42:41 -0500

    Added login and navbar

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const apiKey = require('../config');
+const mysql = require('mysql');
+const config = require('../config');
+const connection = mysql.createConnection(config.db);
+connection.connect();
+
 const apiBaseUrl = 'http://api.themoviedb.org/3';
 const imgBaseUrl = 'http://image.tmdb.org/t/p/w300';
-const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${config.apiKey}`;
 
 //$.getJSON(nowPlayingUrl,()=>{}); This doesn't work in node/express!
 const request = require('request');
@@ -25,5 +29,30 @@ router.get('/', function(req, res, next) {
 router.get('/search',function(req,res,next){
   res.render('search');
 })
+
+router.post('/search/movie',(req,res,next)=>{
+  /*
+  Submitted data from forms comes in the req object
+  Query string data is in req.query
+  Posted data is in req.body
+  */
+  res.json(req.body);
+})
+
+router.get('/login',(req,res,next)=>{
+  res.render('login');
+});
+
+router.post('/loginProcess',(req,res,next)=>{
+  const insertQuery = `INSERT into users (email,password)
+    VALUES (?,?);`;
+  connection.query(insertQuery,[req.body.email,req.body.password],(err,results)=>{
+    if(err){throw err;}
+    else{
+      res.json("Success!");
+    }
+  });
+  res.json(req.body);
+});
 
 module.exports = router;
